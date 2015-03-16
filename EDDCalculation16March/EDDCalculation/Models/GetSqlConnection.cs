37bc7f1296f3ca16c92543f464071389dafe2b31
abhi717099 <abhi_agarwal@dell.com>
2015-03-16 17:25:35 +0530

    EDD Accuracy (16/3/2015)

---
+++ EDDCalculation16March/EDDCalculation/Models/GetSqlConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EDDCalculation.Models
+{
+    public class GetSqlConnection
+    {
+        public static SqlConnection getSqlConnection()
+        {
+            String connString;
+            SqlConnection conn=null;
+            try
+            {
+                connString="Data Source=(LocalDB)\\v11.0;AttachDbFilename=\"C:\\Users\\Abhi_Agarwal\\documents\\visual studio 2013\\Projects\\EDDCalculation\\EDDCalculation\\App_Data\\HolidayDatabase.mdf\";Integrated Security=True";
+                conn = new SqlConnection(connString);
+            }
+            catch { }
+            return conn;
+        }
+    }
+}
---
+++ EDDCalculation16March/EDDCalculation/Models/iSqlOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDDCalculation.Models
+{
+    interface iSqlOperations
+    {
+        void insertHoliday();
+        void deleteHoliday();
+        corporateHolidayClass readEntry();
+        void updateHoliday();
+    }
+}
